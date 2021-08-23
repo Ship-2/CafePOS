13e98771e3586d818fa1f80b6e9f277b11b07bc9
jiwon <jiwon@DESKTOP-K28P42Q>
2021-08-23 19:24:25 +0900

    210823_19:23 김지원 commit
    dailysalse GUI 관련기능

---
+++ src/org/ship2/model/service/DailySalesGuiService.java
@@ -0,0 +1,27 @@
+package org.ship2.model.service;
+
+import static org.ship2.common.JDBCTemplate.close;
+import static org.ship2.common.JDBCTemplate.getConnection;
+
+import java.sql.Connection;
+import java.util.List;
+
+import org.ship2.model.dao.DailySalesGuiDAO;
+import org.ship2.model.dto.DailySalesDTO;
+
+public class DailySalesGuiService {
+	
+	private DailySalesGuiDAO dailySalesDAO = new DailySalesGuiDAO();
+
+	public List<DailySalesDTO> selectDailySales(String startDate, String endDate) {
+
+		Connection con = getConnection();
+		
+		List<DailySalesDTO> dailySalesList = dailySalesDAO.selectDailySales(con, startDate, endDate);
+		
+		close(con);
+		
+		return dailySalesList;
+	}
+
+}
