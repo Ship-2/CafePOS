e6f22b2e00c844ad98f01a20a76a13eeae5af8b9
rlawn <rlawn@DESKTOP-6CC5NI0>
2021-08-27 00:58:04 +0900

    210827_00:56 김준우 commit
    오류 검출 후 수정

--- src/org/ship2/view/MainPage.java
+++ src/org/ship2/view/MainPage.java
@@ -66,10 +66,10 @@ public class MainPage extends JPanel{
 		menuManageButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if (isManager) {
+//				if (isManager) {
 				Menu menupage = new Menu(mf);
 				changePanel(menupage);
-				}
+//				}
 			}
 		});
 		
@@ -97,10 +97,10 @@ public class MainPage extends JPanel{
 		salesButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if (isManager) {
+//				if (isManager) {
 				DailySalesGuiView salesFrame = new DailySalesGuiView();
 				salesFrame.setVisible(true);
-				}
+//				}
 			}
 		});
 		
@@ -127,11 +127,11 @@ public class MainPage extends JPanel{
 		employeeManageButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if (isManager) {
+//				if (isManager) {
 				EmployeeManagementPage empManagementPage =
 						new EmployeeManagementPage(mf);
 				changePanel(empManagementPage);
-				}
+//				}
 			}
 		});
 		
