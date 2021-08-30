968e2dde2dec31c8de518fd2c56967c3eb78fb70
smksm <smksm@211.197.32.122>
2021-08-30 20:28:01 +0900

    20210830_20:27 Application 이름 수정

--- src/org/ship2/run/Application.java
+++ src/org/ship2/run/Application.java
@@ -3,7 +3,7 @@ package org.ship2.run;
 import org.ship2.view.MainFrame;
 import org.ship2.view.Menu;
 
-public class SunminApplication {
+public class Application {
 
 	public static void main(String[] args) {
 		new MainFrame();
